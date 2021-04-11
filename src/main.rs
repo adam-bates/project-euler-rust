46b983491a08219e5975f31da9d073ba25f8c7ec
Adam Bates <adam@adambates.ca>
2021-04-11 11:51:46 -0400

    Extracted parsing args to own function.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 mod problems;
 
 use problems::problem_1;
+use std::env;
 
 pub type Result<Type = ()> = std::result::Result<Type, String>;
 
-fn main() -> Result {
-    let problem = std::env::args()
-        .skip(1)
+fn parse_problem_from_args(args: env::Args) -> Result<usize> {
+    args.skip(1)
         .take(1)
         .find(|_| true)
         .ok_or_else(|| "Please pass in a problem to run. ie. cargo run --release -- 1".to_string())?
         .parse::<usize>()
         .or_else(|_| {
             Err("Please pass in a problem to run. ie: cargo run --release -- 1".to_string())
-        })?;
+        })
+}
+
+fn main() -> Result {
+    let problem = parse_problem_from_args(env::args())?;
 
     let result = match problem {
         1 => problem_1::solve(problem_1::Options::default()),
