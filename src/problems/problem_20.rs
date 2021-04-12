6334557be05465d0c6c490e1aa04a05bb391c7f3
Adam Bates <adam@adambates.ca>
2021-04-12 19:23:19 -0400

    Added unsolved problems 16-20

---
+++ src/problems/problem_20.rs
@@ -0,0 +1,25 @@
+use super::Result;
+
+/*
+Problem 20:
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+Answer:
+*/
+
+pub struct Options {}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+pub fn solve(options: Options) -> Result<usize> {
+    Ok(0)
+}
