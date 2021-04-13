f821102b9c29847d17930fd4cc31fb7825aacf8c
Adam Bates <adam@adambates.ca>
2021-04-12 20:03:33 -0400

    Solved problem 16.

--- src/problems/problem_16.rs
+++ src/problems/problem_16.rs
@@ -6,17 +6,46 @@ Problem 16:
 
 What is the sum of the digits of the number 2^1000?
 
-Answer:
+Answer: 1366
 */
 
-pub struct Options {}
+pub struct Options {
+    n: usize,
+}
 
 impl Default for Options {
     fn default() -> Self {
-        Self {}
+        Self { n: 1000 }
     }
 }
 
 pub fn solve(options: Options) -> Result<usize> {
-    Ok(0)
+    // store 2^n as an array of its digits, allowing us to go above u128's max
+    let mut digits: Vec<usize> = vec![2];
+
+    // each loop multiplies by 2
+    for _ in 1..options.n {
+        let mut carry = false;
+
+        digits.iter_mut().for_each(|digit| {
+            *digit *= 2;
+
+            if carry {
+                *digit += 1;
+            }
+
+            carry = *digit >= 10;
+            if carry {
+                *digit %= 10;
+            }
+        });
+
+        if carry {
+            digits.push(1);
+        }
+    }
+
+    let sum = digits.iter().sum();
+
+    Ok(sum)
 }
