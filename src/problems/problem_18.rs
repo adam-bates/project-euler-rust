492c8501add62b2e11bcbb07356a7308565a8094
Adam Bates <adam@adambates.ca>
2021-04-14 20:06:00 -0400

    Solved problem 18.

--- src/problems/problem_18.rs
+++ src/problems/problem_18.rs
@@ -32,17 +32,62 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route.
       However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
-Answer:
+Answer: 1074
 */
 
-pub struct Options {}
+pub struct Options {
+    pyramid: Vec<Vec<u8>>,
+}
 
 impl Default for Options {
     fn default() -> Self {
-        Self {}
+        Self {
+            pyramid: vec![
+                vec![75],
+                vec![95, 64],
+                vec![17, 47, 82],
+                vec![18, 35, 87, 10],
+                vec![20, 04, 82, 47, 65],
+                vec![19, 01, 23, 75, 03, 34],
+                vec![88, 02, 77, 73, 07, 63, 67],
+                vec![99, 65, 04, 28, 06, 16, 70, 92],
+                vec![41, 41, 26, 56, 83, 40, 80, 70, 33],
+                vec![41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+                vec![53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+                vec![70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+                vec![91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+                vec![63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+                vec![04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23],
+            ],
+        }
     }
 }
 
 pub fn solve(options: Options) -> Result<usize> {
-    Ok(0)
+    get_max_sum_from(&options.pyramid, 0, 0, 0)
+        .ok_or_else(|| "Couldn't get max sum from pyramid".to_string())
+}
+
+fn get_max_sum_from(
+    pyramid: &Vec<Vec<u8>>,
+    parent_sum: usize,
+    i: usize,
+    j: usize,
+) -> Option<usize> {
+    let value = pyramid[i][j] as usize;
+    let sum = parent_sum + value;
+
+    if pyramid.len() <= i + 1 {
+        return Some(sum);
+    }
+
+    let left = get_max_sum_from(pyramid, sum, i + 1, j);
+    let right = get_max_sum_from(pyramid, sum, i + 1, j + 1);
+
+    match (left, right) {
+        (Some(left), Some(right)) => Some(left.max(right)),
+        (Some(left), None) => Some(left),
+        (None, Some(right)) => Some(right),
+        (None, None) => None,
+    }
 }
