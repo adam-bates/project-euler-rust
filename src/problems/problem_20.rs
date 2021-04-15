ccffa57a1f3c429bf4309a1a6e1d7ffbe80b1091
Adam Bates <adam@adambates.ca>
2021-04-14 20:42:44 -0400

    Solved problem 20.

--- src/problems/problem_20.rs
+++ src/problems/problem_20.rs
@@ -9,17 +9,66 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
 
-Answer:
+Answer: 648
 */
 
-pub struct Options {}
+pub struct Options {
+    n: usize,
+}
 
 impl Default for Options {
     fn default() -> Self {
-        Self {}
+        Self { n: 100 }
     }
 }
 
 pub fn solve(options: Options) -> Result<usize> {
-    Ok(0)
+    // store n! as an array of its digits, allowing us to go above u128's max
+    let mut digits: Vec<usize> = vec![1];
+
+    // each loop multiplies by n
+    for n in 2..(options.n + 1) {
+        let mut carry = 0;
+
+        digits.iter_mut().for_each(|digit| {
+            *digit = (*digit * n) + carry;
+
+            carry = if *digit < 10 {
+                0
+            } else {
+                (*digit - (*digit % 10)) / 10
+            };
+
+            *digit %= 10;
+        });
+
+        if carry > 0 {
+            digits.push(carry);
+
+            // Handle carry >= 10
+            {
+                let mut carry = 0;
+
+                digits.iter_mut().for_each(|digit| {
+                    *digit = *digit + carry;
+
+                    carry = if *digit < 10 {
+                        0
+                    } else {
+                        (*digit - (*digit % 10)) / 10
+                    };
+
+                    *digit %= 10;
+                });
+
+                if carry > 0 {
+                    digits.push(carry);
+                }
+            }
+        }
+    }
+
+    let sum = digits.iter().sum();
+
+    Ok(sum)
 }
